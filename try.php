5770d72b08440c0ac156e010135f5bc7a8e3d968
echotamondong <shinoecho@gmail.com>
2023-10-12 17:29:48 +0800

    trying live server

---
+++ try.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    echo "hatdog";
+    echo "123456";
+    echo "<br>";
+    echo "hatdog";
+
+
+    ?>
+</body>
+</html>
